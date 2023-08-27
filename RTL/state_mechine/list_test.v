686889f6308a02d1ac17fb07b1ce4812f537f7dd
moerjie <3165109406@qq.com>
2023-08-27 12:06:12 +0800

    8.27

--- RTL/state_mechine/list_test.v
+++ RTL/state_mechine/list_test.v
@@ -10,41 +10,31 @@ input RET;
 input DATA_IN;
 output OUT;
 
-parameter s0=0;
-parameter s1=1;
-parameter s2=2;
-parameter s3=3;
-parameter s4=4;
+parameter s0=3'd0;
+parameter s1=3'd1;
+parameter s2=3'd2;
+parameter s3=3'd3;
+parameter s4=3'd4;
 
-reg [1:0]stateR;
-reg [1:0]next_state;
+reg [2:0]stateR;
+reg [2:0]next_state;
 reg OUT;
 
 always @ (DATA_IN or stateR) begin
     case(stateR)
-        s0:begin
-            if(DATA_IN) next_state=s1;
-            else next_state = s0;     
-        end
+        s0: next_state<=DATA_IN?s1 : s0;
 
-        s1: begin
-            if(DATA_IN) next_state=s0;
-            else next_state=s2; 
-        end
+        s1: next_state<=DATA_IN?s1:s2;
 
-        s2: begin
-            if(DATA_IN) next_state=s3;
-            else next_state=s0;  
-        end
+        s2: next_state<=DATA_IN?s3:s0;
 
-        s3: begin
-            if(DATA_IN) next_state=s4;
-            else next_state=s2;  
-        end
+        s3: next_state<=DATA_IN?s4:s1;
 
-        s4: begin
-            if(DATA_IN) next_state=s0;
-        end
+        s4: next_state<=DATA_IN?s1:s2;
+
+        default: next_state=s1;
+    endcase
+end
 
 always @(stateR) begin
     if(stateR==s4)
@@ -53,7 +43,7 @@ always @(stateR) begin
         OUT=1'b0;
 end
 
-always @(posedge clk or posedge RET) begin
+always @(posedge CLK or posedge RET) begin
     if(RET)
         stateR<=s0;
     else
