0d3ec8f7c3594bf390f4ee7f5b7c2a9482d7e3e7
Moerjie <sawenmoerjie@outlook.com>
2023-10-28 10:43:21 +0800

    10.28

---
+++ DDS/I2S/hdl_prj/hdlsrc/rec_i2s/clearHighlightingRemovedDeadBlocks.m
@@ -0,0 +1,1 @@
+SLStudio.Utils.RemoveHighlighting(get_param('rec_i2s','Handle'));
---
+++ DDS/I2S/hdl_prj/hdlsrc/rec_i2s/highlightRemovedDeadBlocks.m
@@ -0,0 +1,6 @@
+cs.HiliteType = 'user1';
+cs.ForegroundColor = 'black';
+cs.BackgroundColor = 'red';
+set_param(0, 'HiliteAncestorsData', cs);
+hilite_system('rec_i2s/REC_I2S/SIPO_16BIT_L/Data Type Conversion','user1');
+hilite_system('rec_i2s/REC_I2S/SIPO_16BIT_R/Data Type Conversion','user1');
